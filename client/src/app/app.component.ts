b4aa0086569ed20a5190aecb7ee396bc842103a0
QuanFX <q.foxy98@gmail.com>
2021-11-01 17:45:32 +0700

    persisting login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { AccountService } from './_services/account.service';
+import { User } from './_model/user';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -11,11 +13,18 @@ export class AppComponent implements OnInit {
   title = 'The dating app';
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.getUsers();
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user') || null!);
+    this.accountService.setCurrentUser(user);
+  }
+
   getUsers() {
     this.http.get("https://localhost:5001/api/users").subscribe(
       response => this.users = response,
